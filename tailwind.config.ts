2d72780af5476485a317afe44b7398f448b3be7b
Júnior Nascimento <junior.nascm@gmail.com>
2023-12-21 01:04:14 -0300

    chore: adding assets and configuring tailwind with font and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,17 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      body: 'Work Sans',
+      default: 'Work Sans',
+      display: 'Work Sans',
     },
+    colors: {
+      'white': 'hsl(0, 0%, 100%)',
+      'light-pink': 'hsl(275, 100%, 97%)',
+      'grayish-purple': 'hsl(292, 16%, 49%)',
+      'dark-purple': 'hsl(292, 42%, 14%)',
+    }
   },
   plugins: [],
 }
